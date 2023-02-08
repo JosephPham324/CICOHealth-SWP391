1f3f4e186ddfa47e82f94d169ee61f76d362713a
JosephPham324 <quangpnce170036@fpt.edu.vn>
2023-02-08 20:21:10 +0700

    Model for tables used in authentication function

--- CICOHealth/src/main/java/bean/HealthInfo.java
+++ CICOHealth/src/main/java/bean/HealthInfo.java
@@ -1,13 +1,215 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package bean;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 /**
  *
  * @author Pham Nhat Quang CE170036 (FPTU CANTHO)
  */
-public class HealthInfo {
+@Entity
+@Table(name = "healthInfo")
+public class HealthInfo implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 10)
+    @Column(name = "userID")
+    private String userID;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "gender")
+    private boolean gender;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "height")
+    private double height;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "weight")
+    private double weight;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "age")
+    private int age;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "activeness")
+    private int activeness;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "tdee")
+    private int tdee;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "dailyCalorie")
+    private double dailyCalorie;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "dailyProtein")
+    private double dailyProtein;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "dailyFat")
+    private double dailyFat;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "dailyCarb")
+    private double dailyCarb;
+    @JoinColumn(name = "userID", referencedColumnName = "userID", insertable = false, updatable = false)
+    @OneToOne(optional = false)
+    private User user;
+
+    public HealthInfo() {
+    }
+
+    public HealthInfo(String userID) {
+        this.userID = userID;
+    }
+
+    public HealthInfo(String userID, boolean gender, double height, double weight, int age, int activeness, int tdee, double dailyCalorie, double dailyProtein, double dailyFat, double dailyCarb) {
+        this.userID = userID;
+        this.gender = gender;
+        this.height = height;
+        this.weight = weight;
+        this.age = age;
+        this.activeness = activeness;
+        this.tdee = tdee;
+        this.dailyCalorie = dailyCalorie;
+        this.dailyProtein = dailyProtein;
+        this.dailyFat = dailyFat;
+        this.dailyCarb = dailyCarb;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public boolean getGender() {
+        return gender;
+    }
+
+    public void setGender(boolean gender) {
+        this.gender = gender;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getActiveness() {
+        return activeness;
+    }
+
+    public void setActiveness(int activeness) {
+        this.activeness = activeness;
+    }
+
+    public int getTdee() {
+        return tdee;
+    }
+
+    public void setTdee(int tdee) {
+        this.tdee = tdee;
+    }
+
+    public double getDailyCalorie() {
+        return dailyCalorie;
+    }
+
+    public void setDailyCalorie(double dailyCalorie) {
+        this.dailyCalorie = dailyCalorie;
+    }
+
+    public double getDailyProtein() {
+        return dailyProtein;
+    }
+
+    public void setDailyProtein(double dailyProtein) {
+        this.dailyProtein = dailyProtein;
+    }
+
+    public double getDailyFat() {
+        return dailyFat;
+    }
+
+    public void setDailyFat(double dailyFat) {
+        this.dailyFat = dailyFat;
+    }
+
+    public double getDailyCarb() {
+        return dailyCarb;
+    }
+
+    public void setDailyCarb(double dailyCarb) {
+        this.dailyCarb = dailyCarb;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (userID != null ? userID.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof HealthInfo)) {
+            return false;
+        }
+        HealthInfo other = (HealthInfo) object;
+        if ((this.userID == null && other.userID != null) || (this.userID != null && !this.userID.equals(other.userID))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "bean.HealthInfo[ userID=" + userID + " ]";
+    }
     
 }
--- CICOHealth/src/main/java/bean/Login.java
+++ CICOHealth/src/main/java/bean/Login.java
@@ -1,13 +1,153 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package bean;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 /**
  *
  * @author Pham Nhat Quang CE170036 (FPTU CANTHO)
  */
-public class Login {
+@Entity
+@Table(name = "login")
+public class Login implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 10)
+    @Column(name = "userID")
+    private String userID;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 255)
+    @Column(name = "username")
+    private String username;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 255)
+    @Column(name = "passwordHash")
+    private String passwordHash;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 255)
+    @Column(name = "passwordSalt")
+    private String passwordSalt;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "isGoogleLogin")
+    private boolean isGoogleLogin;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "isBanned")
+    private boolean isBanned;
+    @JoinColumn(name = "userID", referencedColumnName = "userID", insertable = false, updatable = false)
+    @OneToOne(optional = false)
+    private User user;
+
+    public Login() {
+    }
+
+    public Login(String userID) {
+        this.userID = userID;
+    }
+
+    public Login(String userID, String username, String passwordHash, String passwordSalt, boolean isGoogleLogin, boolean isBanned) {
+        this.userID = userID;
+        this.username = username;
+        this.passwordHash = passwordHash;
+        this.passwordSalt = passwordSalt;
+        this.isGoogleLogin = isGoogleLogin;
+        this.isBanned = isBanned;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+
+    public String getPasswordSalt() {
+        return passwordSalt;
+    }
+
+    public void setPasswordSalt(String passwordSalt) {
+        this.passwordSalt = passwordSalt;
+    }
+
+    public boolean getIsGoogleLogin() {
+        return isGoogleLogin;
+    }
+
+    public void setIsGoogleLogin(boolean isGoogleLogin) {
+        this.isGoogleLogin = isGoogleLogin;
+    }
+
+    public boolean getIsBanned() {
+        return isBanned;
+    }
+
+    public void setIsBanned(boolean isBanned) {
+        this.isBanned = isBanned;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (userID != null ? userID.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Login)) {
+            return false;
+        }
+        Login other = (Login) object;
+        if ((this.userID == null && other.userID != null) || (this.userID != null && !this.userID.equals(other.userID))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "bean.Login[ userID=" + userID + " ]";
+    }
     
 }
