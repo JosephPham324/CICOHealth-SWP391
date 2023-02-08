c707ab43d71a80fec4a50bcb9d3a21ccb82b30e3
JosephPham324 <quangpnce170036@fpt.edu.vn>
2023-02-08 07:55:42 +0700

    Setup for Authentication

--- CICOHealth/src/main/java/util/AuthenticationLogic.java
+++ CICOHealth/src/main/java/util/AuthenticationLogic.java
@@ -1,9 +1,19 @@
 package util;
-
+import javax.crypto.SecretKey;
+import util.Encryption;
 /**
  *
  * @author Pham Nhat Quang CE170036 (FPTU CANTHO)
  */
 public class AuthenticationLogic {
+    public String getLoginSalt(String username,String password){
+        return null;
+    }
+    private static SecretKey getSecretKey(String password,String salt){
+        return null;
+    }
     
+    public String encryptPassword(String username,String password){
+        return null;
+    }
 }
