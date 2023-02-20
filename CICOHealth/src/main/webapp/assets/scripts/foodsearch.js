46bb76bc48218023da91955cf40aa8e901bae7d1
JosephPham324 <quangpnce170036@fpt.edu.vn>
2023-02-20 13:55:52 +0700

    View meal feature (not styled)

--- CICOHealth/src/main/webapp/assets/scripts/foodsearch.js
+++ CICOHealth/src/main/webapp/assets/scripts/foodsearch.js
@@ -131,6 +131,17 @@ function removeFood(food) {
   }
   throw new Error("Food not in selected foods");
 }
+//Remove food from selected foods
+function removeFoodByName(foodName) {
+  //Check if food is already in selected foods
+  for (let i = 0; i < selectedFoods.length; i++) {
+    if (selectedFoods[i].foodName === foodName) {
+      selectedFoods.splice(i, 1);
+      return;
+    }
+  }
+  throw new Error("Food not in selected foods");
+}
 //Toggle food in selected foods
 function toggleFood(food) {
   //Check if food is already in selected foods
@@ -155,15 +166,14 @@ function addSearchResultEventListener() {
     //Add event listener to search result
     searchResult.addEventListener("click", function () {
       let food = JSON.parse(this.dataset.food);
-      console.log(food)
       if (toggleFood(food) == true) {
         this.classList.add("selected");
+        changeButtonText(this, "add");
       } else {
         this.classList.remove("selected");
+        changeButtonText(this, "remove");
       }
-      changeButtonText(this);
       updateFoodCartButton();
-      console.log(selectedFoods);
     });
   }
 }
@@ -172,12 +182,22 @@ function addSearchResultEventListener() {
  *
  * @param {Element} searchResult - The search result element
  */
-function changeButtonText(searchResult) {
+function changeButtonText(searchResult, state) {
   let button = searchResult.children[1].children[1].children[5];
-  if (button.innerText === "Add") {
-    button.innerText = "Remove";
-  } else {
-    button.innerText = "Add";
+  // if (button.innerText === "Add") {
+  //   button.innerText = "Remove";
+  // } else {
+  //   button.innerText = "Add";
+  // }
+  switch (state) {
+    case "add":
+      button.innerText = "Remove";
+      break;
+    case "remove":
+      button.innerText = "Add";
+      break;
+    default:
+      break;
   }
 }
 /**
@@ -194,7 +214,33 @@ function showSelected(selectedFoods) {
       )
     ) {
       searchResult.classList.add("selected");
-      changeButtonText(searchResult);
+      changeButtonText(searchResult, "add");
+      updateFoodCartButton();
+    } else {
+      searchResult.classList.remove("selected");
+      changeButtonText(searchResult, "remove");
+      updateFoodCartButton();
+    }
+  }
+}
+/**
+ * Add selected class to search-result elements that contain food in selectedFoods
+ */
+function showSelected() {
+  let searchResults = document.getElementById("search-results");
+  for (let i = 0; i < searchResults.children.length; i++) {
+    let searchResult = searchResults.children[i];
+    if (
+      selectedFoods.find(
+        (food) => food.foodName === searchResult.children[0].innerText //If a selected food has name equal to search result name
+      )
+    ) {
+      searchResult.classList.add("selected");
+      changeButtonText(searchResult, "add");
+      updateFoodCartButton();
+    } else {
+      searchResult.classList.remove("selected");
+      changeButtonText(searchResult, "remove");
       updateFoodCartButton();
     }
   }
@@ -206,58 +252,58 @@ function updateFoodCartButton() {
 addSearchResultEventListener();
 
 //Send request of some common foods, separated by new line
-// sendRequest(`Apple
-//       Banana
-//       Orange
-//       Watermelon
-//       Kiwi
-//       Carrot
-//       Mushroom
-//       Potato
-//       Sweet potato
-//       Corn
-//       Broccoli
-//       Cauliflower
-//       Cabbage
-//       Spinach
-//       Lettuce
-//       Kale
-//       Onion
-//       Garlic
-//       Ginger
-//       Green beans
-//       Peas
-//       Lentils
-//       Chickpeas
-//       Kidney beans
-//       Black beans
-//       Pinto beans
-//       Tuna
-//       Salmon
-//       Shrimp
-//       Chicken
-//       Eggs
-//       Milk
-//       Yogurt
-//       Cheese
-//       Honey
-//       Maple syrup
-//       Bread
-//       Bagel
-//       Oatmeal
-//       Granola
-//       Pasta
-//       Rice
-//       Quinoa
-//       Couscous
-//       Pizza
-//       Burger
-//       Hot dog
-//       Sandwich
-//       Tacos
-//       Burritos
-//       Sushi
-//       Pad Thai`);
+sendRequest(`Apple
+      Banana
+      Orange
+      Watermelon
+      Kiwi
+      Carrot
+      Mushroom
+      Potato
+      Sweet potato
+      Corn
+      Broccoli
+      Cauliflower
+      Cabbage
+      Spinach
+      Lettuce
+      Kale
+      Onion
+      Garlic
+      Ginger
+      Green beans
+      Peas
+      Lentils
+      Chickpeas
+      Kidney beans
+      Black beans
+      Pinto beans
+      Tuna
+      Salmon
+      Shrimp
+      Chicken
+      Eggs
+      Milk
+      Yogurt
+      Cheese
+      Honey
+      Maple syrup
+      Bread
+      Bagel
+      Oatmeal
+      Granola
+      Pasta
+      Rice
+      Quinoa
+      Couscous
+      Pizza
+      Burger
+      Hot dog
+      Sandwich
+      Tacos
+      Burritos
+      Sushi
+      Pad Thai`);
 
 //Food cart element
 let mealCart = document.getElementById("food-cart");
@@ -274,10 +320,24 @@ function displayPopUp(id) {
 //Add event listener to food cart button
 document.getElementById("food-cart").addEventListener("click", displayMealForm);
 
+/**
+ * Display meal form pop-up
+ */
 function displayMealForm() {
   displayPopUp("meal-pop-up");
   let mealForm = document.getElementById("meal-form");
   mealForm.innerHTML = "";
+  //Add meal name input
+  let htmlMealName = `
+  <div class="form-group row">
+    <label for="meal-name" class="col-4 col-form-label">Meal Name</label>
+    <div class="col-8">
+      <input type="text" class="form-control" id="meal-name" placeholder="Meal Name" required = "required">
+    </div>
+  </div>
+  `;
+  mealForm.innerHTML += htmlMealName;
+  //Iterate through selected foods
   selectedFoods.forEach((food) => {
     let elementHTML = `
   <div class="form-group row" class="food-item">
@@ -289,7 +349,7 @@ function displayMealForm() {
             <span class="food-item-carbs">Carbs: ${food.carbs}</span>
             <span class="food-item-serving-weight">Serving: ${food.servingWeight}</span>
           </label>
-          <div class="col-3">
+          <div class="col-2">
             <input
               id="weight-${food.foodName}"
               name="weight-${food.foodName}"
@@ -299,6 +359,16 @@ function displayMealForm() {
               value="${food.actualWeight}"
               oninput="updateFoodItem('${food.foodName}', this.value)"
             />
+          </div>
+          <!--Delete button-->
+          <div class="col-1">
+            <button
+              type="button"
+              class="btn btn-danger"
+              onclick="removeFoodByName('${food.foodName}');displayMealForm();showSelected();"
+            >
+              <i class="fas fa-trash-alt"></i>
+            </button>
           </div>
   </div>
   `;
@@ -314,13 +384,46 @@ function displayMealForm() {
     </button>
   </div>
   </div>
-`;
+  `;
 }
 
 //Update food item in selectedFoods
-function updateFoodItem(foodName,value) {
+function updateFoodItem(foodName, value) {
   let food = selectedFoods.find((food) => food.foodName === foodName);
   //Parse value to integer
   food.actualWeight = parseInt(value);
-  console.log(selectedFoods);
+  let id = `weight-${food.foodName}`;
+  //Label selector for food item
+  let labelSelector = `label[for="${id}"]`;
+  //Fields to update: calories, protein, fat, carbs
+  let calories = document.querySelector(`${labelSelector} .food-item-calories`);
+  let protein = document.querySelector(`${labelSelector} .food-item-protein`);
+  let fat = document.querySelector(`${labelSelector} .food-item-fat`);
+  let carbs = document.querySelector(`${labelSelector} .food-item-carbs`);
+  //Update fields, multiply by ratio of actual weight to serving weight, two decimal places
+  calories.innerText = `Kcal: ${(
+    food.calories *
+    (food.actualWeight / food.servingWeight)
+  ).toFixed(0)}`;
+  protein.innerText = `Protein: ${(
+    food.protein *
+    (food.actualWeight / food.servingWeight)
+  ).toFixed(2)}`;
+  fat.innerText = `Fat: ${(
+    food.fat *
+    (food.actualWeight / food.servingWeight)
+  ).toFixed(2)}`;
+  carbs.innerText = `Carbs: ${(
+    food.carbs *
+    (food.actualWeight / food.servingWeight)
+  ).toFixed(2)}`;
+}
+
+//Submit meal form
+function requestLogCreation(){
+  let formParams = {
+    mealName: document.getElementById("meal-name").value,
+    foods: selectedFoods
+  }
+  post("/CICOHealth/meal-logs", formParams);
 }
