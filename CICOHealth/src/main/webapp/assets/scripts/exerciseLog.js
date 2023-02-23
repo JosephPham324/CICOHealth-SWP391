73a837c42057b9f57390004ec545de68fef0f582
khanhldce <khanhldce160277@fpt.edu.vn>
2023-02-23 11:04:45 +0700

    Create file scss

--- CICOHealth/src/main/webapp/assets/scripts/exerciseLog.js
+++ CICOHealth/src/main/webapp/assets/scripts/exerciseLog.js
@@ -8,3 +8,31 @@ datePicker.addEventListener("change", function () {
     const selectedDate = new Date(datePicker.value);
     console.log(selectedDate);
 });
+
+
+// pop-up 
+const cancelButton = document.querySelectorAll("#btn-cancel");
+const editButtons = document.querySelectorAll('.btn-edit-pop-up');
+const noteButtons = document.querySelectorAll('.btn-note-pop-up');
+const editOverlay = document.querySelector('.edit-overlay');
+const noteOverlay = document.querySelector('.note-overlay');
+
+
+editButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        editOverlay.style.display = 'block';
+    });
+});
+
+noteButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        noteOverlay.style.display = 'block';
+    });
+});
+
+cancelButton.forEach(button => {
+    button.addEventListener('click', () => {
+        noteOverlay.style.display = 'none';
+        editOverlay.style.display = 'none';
+    });
+});
