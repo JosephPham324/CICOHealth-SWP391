089ca683cc4cc973974a827d63e70b94fbdd18fe
Quanvo313 <quanvhce160078@fpt.edu.vn>
2023-03-04 08:04:13 +0700

    fix eyeToggle

--- CICOHealth/src/main/webapp/assets/scripts/loginInfo.js
+++ CICOHealth/src/main/webapp/assets/scripts/loginInfo.js
@@ -1,20 +1,9 @@
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/JavaScript.js to edit this template
- */
-
-
-document.addEventListener('DOMContentLoaded', function () {
-    const togglePassword = document.querySelector('#togglePassword');
-    const password = document.querySelector('#id_password');
-    function eyeToggle() {
-        // toggle the type attribute
-        const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
-        password.setAttribute('type', type);
-        // toggle the eye slash icon
-        this.classList.toggle('fa-eye-slash');
-        var passwordHash = togglePassword.dataset.passwordHash;
-        console.log(passwordHash); // or use the value of passwordHash as needed
-
-    }
-});
+const togglePassword = document.querySelector('#togglePassword');
+const password = document.querySelector('#id_password');
+function eyeToggle() {
+    // toggle the type attribute
+    const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+    password.setAttribute('type', type);
+    // toggle the eye slash icon
+    this.classList.toggle('fa-eye-slash');
+}
