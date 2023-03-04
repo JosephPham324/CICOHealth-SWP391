793f8c78da81a14453ac668688d8dd938876bc55
JosephPham324 <quangpnce170036@fpt.edu.vn>
2023-03-04 11:01:32 +0700

    Some functions for resistance exercise statistics

--- CICOHealth/src/main/webapp/assets/scripts/modules/statistics.js
+++ CICOHealth/src/main/webapp/assets/scripts/modules/statistics.js
@@ -24,6 +24,7 @@ async function fetchData(type, startDate, endDate) {
   await fetch(url)
     .then((response) => response.json())
     .then((data) => {
+      //   console.log(JSON.stringify(data.logs));
       responseData = data;
     });
   //Return data
@@ -57,48 +58,174 @@ function analyzeLogDataByDate(logsCollection, attribute) {
   }
   return dateData;
 }
+//----------------------------------------------------------------------------------------------
+/*----------------Exercise statistics-----------------------*/
+//----------------Utility functions
+function getWeekEndpoints(year, weekNumber) {
+  const januaryFirst = new Date(year, 0, 1);
+  const dayOfWeek = januaryFirst.getDay();
+  const daysInFirstWeek = 7 - dayOfWeek;
+  const firstWeekEnd = new Date(year, 0, daysInFirstWeek);
+  const daysInWeek = 7;
+  const startDate = new Date(
+    firstWeekEnd.getTime() + (weekNumber - 2) * daysInWeek * 86400000
+  );
+  const endDate = new Date(startDate.getTime() + 6 * 86400000);
+  return { startDate, endDate };
+}
+function getWeekNumber(date) {
+  const onejan = new Date(date.getFullYear(), 0, 1);
+  const millisecsInDay = 86400000;
+  return Math.ceil(
+    ((date - onejan) / millisecsInDay + onejan.getDay() + 1) / 7
+  );
+}
+//----------------------------------------------------------------------------------------------
+//----------------Main functions
+function countExercisesPerWeek(data) {
+  const exerciseCounts = {};
 
-function analyzeTopWeightData(logsCollection) {
-  let datePrevious = null;
-  let dateCurrent = null;
-  let dateData = [];
-  let currentDateData = null;
-  for (let i = 0; i < logsCollection.length; i++) {
-    let log = logsCollection[i];
-    dateCurrent = log.logDate;
-    let weights = separateString(log.weight, ",");
-    let topWeight = getMaxValue(weights);
-    if (datePrevious !== dateCurrent) {
-      //Add previous date data to date data
-      if (currentDateData !== null) dateData.push(currentDateData);
-      //Reset date data
-      currentDateData = {
-        date: dateCurrent,
-        data: topWeight,
+  data.forEach((log) => {
+    const logDate = new Date(log.logDate);
+    const year = logDate.getFullYear();
+    const week = getWeekNumber(logDate);
+    const exerciseName = log.exercise.exerciseName;
+
+    if (!exerciseCounts[year]) {
+      exerciseCounts[year] = {};
+    }
+
+    if (!exerciseCounts[year][week]) {
+      exerciseCounts[year][week] = {
+        dateRange: getWeekEndpoints(year, week),
       };
-      datePrevious = dateCurrent;
-      continue;
     }
-    if (log.weight > currentDateData.data) {
-      currentDateData.data = topWeight;
+
+    if (!exerciseCounts[year][week][exerciseName]) {
+      exerciseCounts[year][week][exerciseName] = 0;
     }
-    datePrevious = dateCurrent;
-    if (i === logsCollection.length - 1) {
-      dateData.push(currentDateData);
+
+    exerciseCounts[year][week][exerciseName]++;
+  });
+
+  return exerciseCounts;
+}
+
+function getExerciseFrequency(exerciseName, data) {
+  const exerciseData = {
+    name: exerciseName,
+    total: 0,
+    weeks: [],
+  };
+
+  for (const year in data) {
+    for (const week in data[year]) {
+      if (data[year][week][exerciseName]) {
+        const startDate = data[year][week].dateRange.startDate;
+        const endDate = data[year][week].dateRange.endDate;
+        const count = data[year][week][exerciseName];
+        exerciseData.total += count;
+        exerciseData.weeks.push({ week, count, startDate, endDate });
+      }
     }
   }
-  return dateData;
+
+  return exerciseData;
 }
 
+/**
+ * Converts a string of numbers separated by a specified separator into an array of integers.
+ *
+ * @param {string} string - The string to be converted into an array.
+ * @param {string} separator - The separator used to split the string into an array.
+ * @returns {number[]} An array of integers extracted from the original string.
+ */
 function separateString(string, separator) {
-    return string.split(separator).map((item) => parseInt(item.trim()));
+  return string.split(separator).map((item) => parseInt(item.trim()));
 }
 
+/**
+ * Returns the maximum value in an array of numbers.
+ *
+ * @param {number[]} array - The array of numbers to search for the maximum value.
+ * @returns {number} The maximum value in the array.
+ */
 function getMaxValue(array) {
-    return Math.max(...array);
+  return Math.max(...array);
+}
+
+/**
+ * Analyzes an array of exercise log data and returns an array of objects containing the name of each exercise and the top sets (weight and corresponding reps) logged for each day.
+ * @param {Object[]} data - An array of exercise log data, each containing an exercise name, a weight string, a rep string, and a date string.
+ * @returns {Object[]} An array of objects containing the name of each exercise and the top sets (weight and corresponding reps) logged for each day.
+ * Each object has the following properties:
+ * exerciseName (string): The name of the exercise.
+ * topSets (Object[]): An array of objects, each containing the date, top weight, and corresponding reps for that day.
+ */
+function getDailyTopSets(data) {
+  // create an empty object to store exercises and their top sets
+  const exercises = {};
+  // create an empty array to store the final result
+  const result = [];
+
+  // loop through each item in the data array
+  data.forEach((item) => {
+    // get the name of the exercise from the item
+    const { exerciseName } = item.exercise;
+    // split the weight string and the rep string into arrays and convert each value to a number
+    const weightArray = separateString(item.weight, "/");
+    const repArray = separateString(item.rep, "/");
+    // get the maximum value from the weight array
+    const weight = getMaxValue(weightArray);
+    // get the corresponding number of reps for the top weight
+    const rep = repArray[weightArray.indexOf(weight)];
+
+    // create an object to store the top weight and the corresponding number of reps
+    const data = {
+      weight: weight,
+      rep: rep,
+    };
+
+    // check if the exercises object already has an entry for this exercise
+    if (!exercises[exerciseName]) {
+      // if not, create a new object for this exercise
+      exercises[exerciseName] = {};
+    }
+
+    // get the date of the log from the item
+    const date = item.logDate;
+    // check if the exercises object already has an entry for this exercise on this date
+    if (!exercises[exerciseName][date]) {
+      // if not, add the data as the top set for this exercise on this date
+      exercises[exerciseName][date] = data;
+    } else if (weight > exercises[exerciseName][date].weight) {
+      // if there is already a top set for this exercise on this date,
+      // update it if the current weight is higher
+      exercises[exerciseName][date] = data;
+    }
+  });
+
+  // loop through each exercise in the exercises object
+  for (const exerciseName in exercises) {
+    // get the top sets for this exercise on each date
+    const topSets = Object.entries(exercises[exerciseName]).map(
+      ([date, data]) => ({ date, weight: data.weight, rep: data.rep })
+    );
+    // add this exercise and its top sets to the result array
+    result.push({ exerciseName, topSets });
+  }
+
+  // return the final result array
+  return result;
 }
 
 //Export fetch data function
-export { fetchData, analyzeLogDataByDate, analyzeTopWeightData };
+export {
+  fetchData,
+  analyzeLogDataByDate,
+  getDailyTopSets,
+  countExercisesPerWeek,
+  getExerciseFrequency
+};
 
 //Date picker html, start and end date
