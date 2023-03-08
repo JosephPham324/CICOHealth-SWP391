3a554f82fa3fabf24979fa23f0e614b2f2bb9e74
Quanvo313 <quanvhce160078@fpt.edu.vn>
2023-03-08 13:03:25 +0700

    update healthinfo statistic

--- CICOHealth/src/main/webapp/assets/scripts/statistics/healthinfostatistics.js
+++ CICOHealth/src/main/webapp/assets/scripts/statistics/healthinfostatistics.js
@@ -1,49 +1,47 @@
-//---------------------------------------------------------------------------------------------
-import {
-    fetchData
-} from "../modules/statistics"
-const startDate = document.getElementById("start-date");
-const endDate = document.getElementById("end-date");
+const uri = "http://localhost:8080/CICOHealth/user/statistics/healthinfo/data";
+let data = "";
 
-//Take both dates
-async function fetchDate() {
-  const startDate = document.getElementById("start-date").value;
-  const endDate = document.getElementById("end-date").value;
-  let data = await fetchData("exercise/cardio", startDate, endDate);
-  return await data;
+async function fetchData() {
+    try {
+        const response = await fetch(uri);
+        if (!response.ok) {
+            throw new Error("Failed to retrieve data from the URI");
+        }
+        const jsonData = await response.json();
+        data = jsonData;
+        handleData(data);
+        displayTotalChart();
+    } catch (error) {
+        console.error(error);
+    }
 }
 
-//Add event listener to both date pickers
-startDate.addEventListener("change", displayData);
-endDate.addEventListener("change", displayData);
+function handleData(data) {
+    console.log(data);
+}
 
-//Initialize end date to today
-endDate.value = new Date().toISOString().substr(0, 10);
-//Initialize start date to 30 days ago
-startDate.value = new Date(new Date().setDate(new Date().getDate() - 30))
-  .toISOString()
-  .substr(0, 10);
+function displayTotalChart() {
+    if (!data) {
+        console.log("Data is not yet available.");
+        return;
+    }
+    const dates = Object.keys(data);
+    const values = Object.values(data);
+    const chart = new Chart(document.getElementById("statistics-chart"), {
+        type: "line",
+        data: {
+            labels: dates,
+            datasets: [{
+                    label: "Calorie",
+                    data: values,
+                    fill: false,
+                    borderColor: "rgb(75, 192, 192)",
+                    backgroundColor: "rgba(75, 192, 192, 0.2)",
+                    tension: 0.1
+                }]
+        },
+        options: {}
+    });
+}
 
-
-
-const hihi = JSON
-
-const data = {"2023-03-06":1854.0,"2023-03-03":2093.7,"2023-03-02":1557.0,"2023-03-04":2556.0};
-
-const dates = Object.keys(data);
-const values = Object.values(data);
-
-const chart = new Chart(document.getElementById("statistics-chart"), {
-  type: "line",
-  data: {
-    labels: dates,
-    datasets: [{
-      label: "Calorie",
-      data: values,
-      fill: false,
-      borderColor: "rgb(75, 192, 192)",
-      tension: 0.1
-    }]
-  },
-  options: {}
-});
+fetchData();
