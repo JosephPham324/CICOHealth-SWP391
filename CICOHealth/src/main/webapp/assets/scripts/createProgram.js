189409297630d22fdf8600aa4ca3721a34f69d62
khanhldce <khanhldce160277@fpt.edu.vn>
2023-03-08 08:09:00 +0700

    Update exercise program create ui

---
+++ CICOHealth/src/main/webapp/assets/scripts/createProgram.js
@@ -0,0 +1,8 @@
+// pop-up 
+const createExerciseButtons = document.querySelectorAll('.btn-create-exercise-pop-up');
+
+createExerciseButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        displayPopUp("create-exercise-pop-up");
+    });
+});
