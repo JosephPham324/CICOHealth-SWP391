a7934b2c217addc4ed319abc1f3edd5c8997919b
Quanvo313 <quanvhce160078@fpt.edu.vn>
2023-03-19 01:11:30 +0700

    add cookies

--- CICOHealth/src/main/java/controller/CentralController.java
+++ CICOHealth/src/main/java/controller/CentralController.java
@@ -1,10 +1,16 @@
 package controller;
 
+import dao.LoginDao;
 import java.io.IOException;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Pham Nhat Quang CE170036 (FPTU CANTHO)
@@ -23,7 +29,7 @@ public class CentralController extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         String URI = request.getRequestURI();
-        
+
         if (URI.endsWith("/register")) {
             request.getRequestDispatcher("RegisterController").forward(request, response);
             return;
@@ -47,6 +53,24 @@ public class CentralController extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals("UserName")) {
+                    try {
+                        request.getSession().setAttribute("user", new LoginDao().getLoginInfoByUsername(cookie.getValue()));
+                        response.sendRedirect("/CICOHealth");
+                    } catch (SQLException ex) {
+                        Logger.getLogger(LoginController.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+
+                }
+                if (cookie.getName().equals("googleID")) {
+                    request.getSession().setAttribute("googleID", cookie.getValue());
+                    response.sendRedirect("/CICOHealth");
+                }
+            }
+        }
 
     }
 
